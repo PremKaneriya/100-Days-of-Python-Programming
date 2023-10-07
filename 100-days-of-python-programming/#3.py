65e44ce8bc969efae1f2a23ecfc24d479a21059c
Prem Kaneriya <premkaneriyappp@gmail.com>
2023-10-07 18:10:20 +0530

    day 3

---
+++ 100-days-of-python-programming/#3.py
@@ -0,0 +1,20 @@
+
+# program 1
+my_list = [1,2,3,4]
+my_list.append(5)
+
+print(my_list)
+
+#program 2
+my_dict = {'key1': 'value1', 'key2': 'value2'}
+print(my_dict['key2'])
+
+#program 3
+for i in range(5):
+    print(i)
+
+#program 4
+new_list = [i + 1 for i in range if (10)]
+
+print(new_list)
+
