3a9da69a96d09dff948ca13ff2023decabccf3fc
Prem Kaneriya <premkaneriyappp@gmail.com>
2023-10-10 17:45:45 +0530

    Day 7

---
+++ #7.py
@@ -0,0 +1,14 @@
+a = 10 
+b = 13
+
+ans1 = a + b
+print("addition of",a,"and",b, "is", ans1 )
+
+ans1 = a * b
+print("multiplication of",a,"and",b, "is", ans1 )
+
+ans1 = a - b
+print("subtraction of",a,"and",b, "is", ans1 )
+
+ans1 = a / b
+print("division of",a,"and",b, "is", ans1 )
