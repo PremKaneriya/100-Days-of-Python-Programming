fa130238f46f2115cad714034b46462228cd6264
Prem Kaneriya <premkaneriyappp@gmail.com>
2023-10-12 18:24:37 +0530

    day 10

---
+++ 100-days-of-python-programming/#10.py
@@ -0,0 +1,10 @@
+#inputs
+
+a = input("Enter first Number: ")
+b = input("Enter second Number ")
+
+print("Addition is",int(a) + int(b))
+print("Subtraction is",int(a) - int(b))
+print("Multiplication is",int(a) * int(b))
+print("Division is",int(a) / int(b))
+print("Modules is",int(a) % int(b))
