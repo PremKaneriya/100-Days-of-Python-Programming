f0a36ded5b3752d9d661f549c438295f45e55a83
Prem Kaneriya <premkaneriyappp@gmail.com>
2023-10-13 17:52:29 +0530

    day 11

---
+++ 100-days-of-python-programming/#11.py
@@ -0,0 +1,18 @@
+#strings
+
+name = "Elon Musk"
+
+address = '''Hello World''' 
+print(address)
+
+print(name)
+
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+
+
+#print using for loop
+for character in name:
+    print(character) 
