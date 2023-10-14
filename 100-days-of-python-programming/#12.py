ad797ed71d2aa6963ab44ad3f9e1cc313399fae6
Prem Kaneriya <premkaneriyappp@gmail.com>
2023-10-14 16:46:47 +0530

    Day-12

---
+++ 100-days-of-python-programming/#12.py
@@ -0,0 +1,14 @@
+#String slicing
+
+animal = "Girrafe"
+len1 = len(animal)
+print("Girrafe in numbers is =", len1)
+
+print(animal[:5])
+
+print(animal[:-4])
+
+num = "12345"
+for n in num:
+    print(n)
+
