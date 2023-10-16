a6badac73a16f07998924c998aa02821cb59a41b
Prem Kaneriya <premkaneriyappp@gmail.com>
2023-10-16 18:40:31 +0530

    day 13

---
+++ 100-days-of-python-programming/#13.py
@@ -0,0 +1,14 @@
+#String Methods
+
+a = "Prem Kaneriya"
+
+print(len(a))
+
+print(a.upper())
+print(a.lower())
+print(a.capitalize())
+print(a.count())
+print(a.islower())
+print(a.isspace())
+
+#and many more
