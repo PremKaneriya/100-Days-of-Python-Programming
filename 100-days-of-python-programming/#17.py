5065b0817cdb24f8427b95dc35a1b1f7101f2ac7
Prem Kaneriya <premkaneriyappp@gmail.com>
2023-10-21 17:39:44 +0530

    update

--- 100-days-of-python-programming/#17.py
+++ 100-days-of-python-programming/#17.py
@@ -1,6 +1,22 @@
 # Basic For loop
 
-colors = ["Red", "Yellow", "Blue", "Green" ]
+# colors = ["Red", "Yellow", "Blue", "Green" ]
 
-for a in colors:
-    print(a)
+# for a in colors:
+#     print(a)
+
+
+# star = ["*", "*", "*"]
+
+# for s in star:
+#     print(s)
+
+# rows = 5
+# for i in range(0, rows):
+#     for j in range(0, i + 1):
+#         print("*", end=' ')
+#     print("\r")
+
+
+for i in range(10):
+    print("13 X", i+1, "=", 13*(i+1) )
