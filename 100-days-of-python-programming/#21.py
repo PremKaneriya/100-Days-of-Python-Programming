8e7ff1f6305d29551c3cb3f7fe619c79d1c5abd6
Prem Kaneriya <premkaneriyappp@gmail.com>
2023-10-23 18:51:56 +0530

    day 21

---
+++ 100-days-of-python-programming/#21.py
@@ -0,0 +1,12 @@
+#function argument
+
+def calculate (a=1, b=3):
+    print("the calculation is =", a + b)
+
+calculate(1, 1)
+#so we change values 
+
+def greet(name, surname):
+    print("Hello", name, surname)
+
+greet("prem", "kaneriya")    
